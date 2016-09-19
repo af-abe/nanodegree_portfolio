d3bd86f9a17bea2ffd4d71ba2b8b54c7995de6bf
ABE <abe+git@appsfactory.de>
2016-09-19 16:14:57 +0200

    [FIX] memory leek

--- app/src/main/java/appsfactory/nanodegree/portfolio/ui/activities/MainActivity.java
+++ app/src/main/java/appsfactory/nanodegree/portfolio/ui/activities/MainActivity.java
@@ -1,6 +1,7 @@
 package appsfactory.nanodegree.portfolio.ui.activities;
 
 import android.databinding.DataBindingUtil;
+import android.databinding.ViewDataBinding;
 import android.os.Bundle;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.Loader;
@@ -16,12 +17,13 @@ import appsfactory.nanodegree.portfolio.models.MainModel;
 
 public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<List<ButtonItemViewModel>> {
     private MainModel mainModel;
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mainModel = new MainModel();
-        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
         binding.setModel(mainModel);
 
         loadContent();
@@ -32,6 +34,13 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
                 .forceLoad();
     }
 
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        //unset adapter to avoid memory leeks
+        binding.recyclerView.setAdapter(null);
+    }
 
     @Override
     public Loader<List<ButtonItemViewModel>> onCreateLoader(int id, Bundle args) {
